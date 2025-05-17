dc9bbd01586bbec4025937b074c7b976711fc887
Owenonroad1014 <dtes95105@gmail.com>
2025-05-17 09:40:58 +0800

    沒有資料的錯誤處理

--- api.ts
+++ api.ts
@@ -5,9 +5,21 @@ import { ITask } from '@/types/task';
 const API_URL = 'https://gym-backend-pi.vercel.app';
 
 export const getAllTodos = async (): Promise<ITask[]> => {
+    try{
     const res = await fetch(`${API_URL}/todo/api`, { cache: 'no-store'});
+    if(!res.ok) {
+        throw new Error('Failed to fetch data');
+
+    }
     const todos = await res.json();
+    if(!todos.rows || todos.rows.length === 0) {
+        return [];
+    }
     return todos.rows;
+}catch (error) {
+    console.error('Error fetching todos:', error);
+    throw error;
+}
 }
 
 
